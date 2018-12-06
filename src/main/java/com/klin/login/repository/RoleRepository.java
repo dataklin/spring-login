ae2fb173a0e92399334a4bf949688c23a0aded3e
dataklin <linkangy@gmail.com>
2018-12-05 19:18:12 -0600

    Initial commit

---
+++ src/main/java/com/klin/login/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.klin.login.repository;
+
+import com.klin.login.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
+
+}
---
+++ src/main/java/com/klin/login/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.klin.login.repository;
+
+import com.klin.login.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
